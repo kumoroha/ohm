ed44979683ba029980222b497d31073eb809c05a
kumoroha <199690710+kumoroha@users.noreply.github.com>
2025-12-17 11:34:36 +0900

    Update ohm.js

--- ohm.js
+++ ohm.js
@@ -5,18 +5,24 @@ let ans;
 let voltbar = document.getElementById('dcv');
 let ampr = document.getElementById('i');
 let vfr = document.getElementById('vf');
+let w;
+const ansar = document.getElementById('ansdiv');
 
 voltbar.focus();
 
 function cal() {
-    voltba = voltbar.value;
-    amp = ampr.value;
-    vf = vfr.value;
-    if (voltba == null || voltba == '' || amp == null || amp = '' || vf == null || vf == '') {
+    voltba = parseFloat(voltbar.value);
+    amp = parseFloat(ampr.value);
+    vf = parseFloat(vfr.value);
+    if (voltba == null || voltba == '' || amp == null || amp == '' || vf == null || vf == '') {
         alert('すべてのテキストボックスに値を入力してください。');
+    } else if (amp == 0) {
+        alert('アンペア数を確認してください。\n本当に0ですか？');
     } else {
         ans = (voltba - vf) / amp;
+        w = vf * amp;
     }
     ansar.textContent = ans;
 }
+
 
